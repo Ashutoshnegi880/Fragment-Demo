eb3b95f98574a3484db7a50885de1dee3ad52405
Ashutoshnegi880 <68495328+Ashutoshnegi880@users.noreply.github.com>
2021-10-02 12:41:22 +0530

    Fragment Demo

---
+++ app/src/main/java/com/example/fragmentdemo/FirstFragment.kt
@@ -0,0 +1,19 @@
+package com.example.fragmentdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_first.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FirstFragment : Fragment(R.layout.fragment_first) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvFirstFragment
+    }
+}
